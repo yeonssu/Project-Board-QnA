76654d84d55854a24fbaa5d9529d2a2dbdbe2463
yeonssu <dustn7197@naver.com>
2023-04-03 22:51:14 +0900

    create: 프로젝트 생성

---
+++ src/test/java/com/qnaserver/QnaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qnaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnaBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
